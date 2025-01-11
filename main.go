115a4cd0fc87cb109a351ee9a9a35b13bed6c5d7
macbook pro <macbookpro@meskinelan.home>
2025-01-11 18:05:14 +0100

    question 3

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+import (
+ "net/http"
+ "github.com/gin-gonic/gin"
+)
+func main() {
+ r := gin.Default()
+ r.GET("/myapp", func(c *gin.Context) {
+   c.JSON(http.StatusOK, gin.H{
+     "ringover": "is awesome",
+}) })
+r.Run() }
