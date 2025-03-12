97561c32b60f68d0d8ee7bd6c5bd380dbaf7715d
Hamadou9203 <135527337+Hamadou9203@users.noreply.github.com>
2025-03-12 12:50:19 +0100

    Update main.go

--- Go/main.go
+++ Go/main.go
@@ -8,7 +8,7 @@ func main() {
  r := gin.Default()
  r.GET("/myapp", func(c *gin.Context) {
    c.JSON(http.StatusOK, gin.H{
-     "ringover": "is awesome",
+     "ring": "is awesome",
 }) 
 })
 // ajout d'une route healthcheck
